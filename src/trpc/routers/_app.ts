a5a7ef505dd7496e256b209b4c7bdd962beebd63
Michael C | lorexcold <123798868+Michael-CodeLabs@users.noreply.github.com>
2025-07-10 19:07:51 +0200

    09: projects

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,10 @@
+import { projectsRouter } from '@/modules/projects/server/procedures';
+import {messageRouter} from "@/modules/messages/server/procedures";
 import {createTRPCRouter } from '../init';
 
-import {messageRouter} from "@/modules/messages/server/procedures";
-
 export const appRouter = createTRPCRouter({
-messages: messageRouter
+messages: messageRouter,
+projects: projectsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
